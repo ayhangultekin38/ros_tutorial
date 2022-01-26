d26b39cb17d8432e87f041aba865315b3394d8bb
ayhangultekin38 <ayhangultekin38@gmail.com>
2022-01-26 22:36:54 +0300

    j

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ayhan/catkin_ws/src/ros_tutorial
+# Build directory: /home/ayhan/catkin_ws/src/ros_tutorial/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ayhan/catkin_ws/src/ros_tutorial/build/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/ayhan/catkin_ws/src/ros_tutorial/build/devel/lib:$LD_LIBRARY_PATH"
+export PATH='/opt/ros/noetic/bin:/home/ayhan/.vscode-server/bin/899d46d82c4c95423fb7e10e68eba52050e30ba3/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/wsl/lib:/mnt/c/WINDOWS/system32:/mnt/c/WINDOWS:/mnt/c/WINDOWS/System32/Wbem:/mnt/c/WINDOWS/System32/WindowsPowerShell/v1.0/:/mnt/c/WINDOWS/System32/OpenSSH/:/mnt/c/Program Files (x86)/NVIDIA Corporation/PhysX/Common:/mnt/c/WINDOWS/system32:/mnt/c/WINDOWS:/mnt/c/WINDOWS/System32/Wbem:/mnt/c/WINDOWS/System32/WindowsPowerShell/v1.0/:/mnt/c/WINDOWS/System32/OpenSSH/:/mnt/c/Program Files/Git/cmd:/mnt/c/Users/ayhan/AppData/Local/Microsoft/WindowsApps:/mnt/c/Users/ayhan/AppData/Local/GitHubDesktop/bin:/mnt/c/Users/ayhan/AppData/Local/Programs/Microsoft VS Code/bin:/snap/bin'
+export PKG_CONFIG_PATH="/home/ayhan/catkin_ws/src/ros_tutorial/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/ayhan/catkin_ws/src/ros_tutorial/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/ayhan/catkin_ws/src/ros_tutorial:$ROS_PACKAGE_PATH"
